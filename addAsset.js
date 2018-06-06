9e6feb4b205d262879671ae550ab1fce8def04d6
gmlwhdtjd <gmlwhdtjd@naver.com>
2018-06-06 21:30:48 +0900

    color & type updata

--- addAsset.js
+++ addAsset.js
@@ -1,108 +1,110 @@
-const fs = require('fs');
-const rl = require('readline-sync');
-const qr = require('qr-image');
-const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs')
+const rl = require('readline-sync')
+const qr = require('qr-image')
+const MongoClient = require('mongodb').MongoClient
 
 if (process.argv.length != 3)
-  console.log("usage: node addAsset.js JsonPath");
+    console.log("usage: node addAsset.js JsonPath")
 else {
-  // read Json file
-  try {
-    var jsonString = fs.readFileSync(process.argv[2]);
-  }
-  catch (err) {
-    console.log("Error reading your Json file" + err);
-  }
-  
-  // Json parsing & Load asset data
-  var processedBsons = [];
-
-  var jsonData = JSON.parse(jsonString);
-  for (var itemIndex in jsonData) {
-    var item = jsonData[itemIndex]
-
-    var itemBson = {};
-    itemBson.brandName = item.brandName;
-    itemBson.itemId = item.itemId;
-    itemBson.itemImage;
-    itemBson.assetList = {};
-    itemBson.qrList = {};
-
-    var assetLoadFlag = true;
-
+    // read Json file
     try {
-      // Item Image load
-      itemImage = fs.readFileSync(item.itemImagePaht) 
-      itemBson.itemImage = itemImage;
+        var jsonString = fs.readFileSync(process.argv[2])
     }
-    catch (error) {
-      console.log("Warning: Item number " + itemIndex + " was excluded during loading of the Image.");
-      console.log("         brandName: " + item.brandName);
-      console.log("         itemId: " + item.itemId);
-      console.log("         File load error: no such file or directory, open '" + item.itemImagePaht + "'");
-      console.log();
-      assetLoadFlag = false;
+    catch (err) {
+        console.log("Error reading your Json file" + err)
+    }
+    
+    // Json parsing & Load asset data
+    var processedBsons = []
+
+    var jsonData = JSON.parse(jsonString)
+    for (var itemIndex in jsonData) {
+        var item = jsonData[itemIndex]
+
+        var itemBson = {}
+        itemBson.brandName = item.brandName
+        itemBson.itemId = item.itemId
+        itemBson.itemType = item.itemType
+        itemBson.itemColor = item.itemColor
+        itemBson.itemImage
+        itemBson.assetList = {}
+        itemBson.qrList = {}
+
+        var assetLoadFlag = true
+
+        try {
+            // Item Image load
+            itemImage = fs.readFileSync(item.itemImagePaht) 
+            itemBson.itemImage = itemImage
+        }
+        catch (error) {
+            console.log("Warning: Item number " + itemIndex + " was excluded during loading of the Image.")
+            console.log("         brandName: " + item.brandName)
+            console.log("         itemId: " + item.itemId)
+            console.log("         File load error: no such file or directory, open '" + item.itemImagePaht + "'")
+            console.log()
+            assetLoadFlag = false
+        }
+
+        for (var sizeName in item.assetPathList) {
+            try {
+                // Asset Data load
+                assetData = fs.readFileSync(item.assetPathList[sizeName]) 
+                itemBson.assetList[sizeName] = assetData
+
+                // Generate QR Code
+                var qrJson = 
+                "{" + 
+                    "\"brandName\":\"" + item.brandName + "\"," +
+                    "\"itemId\":\"" + item.itemId + "\"," +
+                    "\"itemSize\":\"" + sizeName + "\"" +
+                "}"
+                itemBson.qrList[sizeName] = qr.imageSync(qrJson)
+            }
+            catch (error) {
+                console.log("Warning: Item number " + itemIndex + " was excluded during loading of the file.")
+                console.log("         brandName: " + item.brandName)
+                console.log("         itemId: " + item.itemId)
+                console.log("         File load error: no such file or directory, open '" + item.assetPathList[sizeName] + "'")
+                console.log()
+                assetLoadFlag = false
+                break
+            }
+        }
+
+        if (assetLoadFlag) {
+            console.log("Item number " + itemIndex + " loaded successfully.")
+            processedBsons.push(itemBson)
+        }
     }
 
-    for (var sizeName in item.assetPathList) {
-      try {
-        // Asset Data load
-        assetData = fs.readFileSync(item.assetPathList[sizeName]) 
-        itemBson.assetList[sizeName] = assetData;
+    // Load Process Check
+    console.log("Total number of loaded Item: " + processedBsons.length)
+    var upload = rl.question("Do you want to upload these items to server? (y/n) ")
 
-        // Generate QR Code
-        var qrJson = 
-        "{" + 
-          "\"brandName\":\"" + item.brandName + "\"," +
-          "\"itemId\":\"" + item.itemId + "\"," +
-          "\"itemSize\":\"" + sizeName + "\"" +
-        "}";
-        itemBson.qrList[sizeName] = qr.imageSync(qrJson);
-      }
-      catch (error) {
-        console.log("Warning: Item number " + itemIndex + " was excluded during loading of the file.");
-        console.log("         brandName: " + item.brandName);
-        console.log("         itemId: " + item.itemId);
-        console.log("         File load error: no such file or directory, open '" + item.assetPathList[sizeName] + "'");
-        console.log();
-        assetLoadFlag = false;
-        break;
-      }
+    // Upload
+    if (upload == "y" || upload == "Y") {
+        MongoClient.connect('mongodb://localhost:27017', function(error, db){
+            if(error) {
+                console.log(error)
+            } else {
+                console.log('MongoDB is connected!')
+
+                const FigureDB = db.db('FigureDB')
+
+                FigureDB.collection('Items').insertMany(processedBsons, function(error,res) {
+                    if(error) {
+                        console.log(error)
+                    } else {    
+                        console.log("The file was saved!")
+                    }
+                })
+
+                db.close()
+            }
+        })
     }
-
-    if (assetLoadFlag) {
-      console.log("Item number " + itemIndex + " loaded successfully.");
-      processedBsons.push(itemBson);
+    else {
+        console.log("Upload is cancelled")
     }
-  }
-
-  // Load Process Check
-  console.log("Total number of loaded Item: " + processedBsons.length);
-  var upload = rl.question("Do you want to upload these items to server? (y/n) ");
-
-  // Upload
-  if (upload == "y" || upload == "Y") {
-    MongoClient.connect('mongodb://localhost:27017', function(error, db){
-      if(error) {
-        console.log(error);
-      } else {
-        console.log('MongoDB is connected!');
-
-        const FigureDB = db.db('FigureDB');
-
-        FigureDB.collection('Items').insertMany(processedBsons, function(error,res) {
-          if(error) {
-            console.log(error);
-          } else {    
-            console.log("The file was saved!");
-          }
-        });
-
-        db.close();
-      }
-    });
-  }
-  else {
-    console.log("Upload is cancelled");
-  }
 }
--- server.js
+++ server.js
@@ -1,58 +1,93 @@
-const express = require('express');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors())
 
-async function getItemsFromDB(query) {
-  let db = await MongoClient.connect('mongodb://localhost:27017');
-  let FigureDB = db.db('FigureDB');
+async function getItemsFromDB(query, field) {
+    let db = await MongoClient.connect('mongodb://localhost:27017')
+    let FigureDB = db.db('FigureDB')
 
-  let cursor = await FigureDB.collection("Items").find(query);
-  let itemList = await cursor.toArray()
-  await db.close();
+    let cursor = await FigureDB.collection("Items").find(query).project(field)
+    let itemList = await cursor.toArray()
+    await db.close()
 
-  return itemList;
+    return itemList
 }
 
 app.get('/api/itemList', (req, res) => {
-  getItemsFromDB()
-  .then(items => {
-    items.map(item => {
-      delete item.assetList;
-    });
-    res.send({ items });
-  });
-});
+    var dbQuery = {}
+    var dbField = {}
+    dbField.brandName = 1
+    dbField.itemId = 1
+    dbField.itemType = 1
+    dbField.itemColor = 1
+    dbField.itemImage = 1
+    dbField.qrList = 1
+
+    getItemsFromDB(dbQuery, dbField)
+    .then(items => {
+        res.send({ items })
+    })
+})
+
+app.get('/api/itemInfo/:itemString', (req, res) => {
+    var itemInfo = req.param("itemString").split("-")
+    
+    var dbQuery = {}
+    dbQuery.brandName = itemInfo[0]
+    dbQuery.itemId = itemInfo[1]
+
+    var dbField = {}
+    dbField.brandName = 1
+    dbField.itemId = 1
+    dbField.itemType = 1
+    dbField.itemColor = 1
+
+    getItemsFromDB(dbQuery, dbField)
+    .then(items => {
+        if(items.length == 1){
+            item = items[0]
+            res.send({ items })
+        }
+        else {
+            console.log("Find false")
+            res.status(500).send('500 File is not exist')
+        }
+    })
+})
 
 app.get('/api/assetBundles/:itemString', (req, res) => {
-  var itemInfo = req.param("itemString").split("-");
-  
-  var dbQuery = {};
-  dbQuery.brandName = itemInfo[0];
-  dbQuery.itemId = itemInfo[1];
+    var itemInfo = req.param("itemString").split("-")
+    
+    var dbQuery = {}
+    dbQuery.brandName = itemInfo[0]
+    dbQuery.itemId = itemInfo[1]
 
-  getItemsFromDB(dbQuery)
-  .then(items => {
-    if(items.length == 1){
-      item = items[0];
-      assetBinary = item.assetList[itemInfo[2]];
-      
-      var filename = itemInfo[0] + '_' + itemInfo[1] + '_' + itemInfo[2];
-      console.log("Find Successed : " + filename);
-      res.setHeader('Content-Disposition', 'attachment; filename=' + filename);
-      res.setHeader('Content-Transfer-Encoding', 'binary');
-      res.setHeader('Content-Type', 'application/octet-stream');
-      res.send(assetBinary.buffer);
-    }
-    else {
-      console.log("Find false")
-      res.status(500).send('500 File is not exist');
-    }
-  });
-});
+    var dbField = {}
+    dbField.assetList = 1
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    getItemsFromDB(dbQuery)
+    .then(items => {
+        if(items.length == 1){
+            item = items[0]
+            assetBinary = item.assetList[itemInfo[2]]
+            
+            var filename = itemInfo[0] + '_' + itemInfo[1] + '_' + itemInfo[2]
+            console.log("Find Successed : " + filename)
+            res.setHeader('Content-Disposition', 'attachment filename=' + filename)
+            res.setHeader('Content-Transfer-Encoding', 'binary')
+            res.setHeader('Content-Type', 'application/octet-stream')
+            res.send(assetBinary.buffer)
+        }
+        else {
+            console.log("Find false")
+            res.status(500).send('500 File is not exist')
+        }
+    })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
